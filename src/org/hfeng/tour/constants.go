83bf4019f84a6dbf7056708f60df1a41bfcea682
Feng <haoran.feng@sap.com>
2015-09-25 01:55:59 -0700

    finish basics

---
+++ src/org/hfeng/tour/constants.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+	const World = "WD"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", World)
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
