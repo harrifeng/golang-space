774e78843d660e76085af84c3dceab6cf588551c
Feng <haoran.feng@sap.com>
2015-09-29 23:28:26 -0700

    errors finish

---
+++ src/org/hfeng/tour/methods/interfaces.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Abser interface {
+	Abs() float64
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f  // MyFloat implement Abser
+	a = &v // *Vertex implement Abser
+
+	// v is Vertex by following sentence (Not *Vertex)
+	// Vertex do not implement Abser
+
+	fmt.Println(a.Abs())
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
